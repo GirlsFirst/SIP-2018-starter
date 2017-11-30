aa5e96cc8404d47fa65682af26396e9db98df001
DianaGWC <diana@girlswhocode.com>
2017-11-30 14:32:17 -0800

    Starter Code.

---
+++ U2-Applications/U2.3-Data/surveyproject-starter.py
@@ -0,0 +1,13 @@
+
+# Creates the dictionary to store responses.
+answers = {}
+
+'''
+Below, write code that will pose the survey questions from the student prompt
+to a user. Your program should save user input as a dictionary.
+'''
+
+
+
+# Print the context of the dictionary.
+print(answers)
