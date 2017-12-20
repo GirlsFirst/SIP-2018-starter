481292ff89a8ddfcd67a643216a3868dd87da73c
DianaGWC <diana@girlswhocode.com>
2017-12-20 13:02:10 -0800

    Starter code that loads the Twitter data and appropriate libraries. Not necessary if students completed the exploration on the previous day.

---
+++ U2-Applications/U2.3-Data/data_vis_project_starter.py
@@ -0,0 +1,16 @@
+'''
+In this project, you will visualize the feelings and language used in a set of
+Tweets. This starter code loads the appropriate libraries and the Twitter data you'll
+need!
+'''
+
+import json
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+#Get the JSON data
+tweetFile = open("../TwitterData/tweets_small.json", "r")
+tweetData = json.load(tweetFile)
+tweetFile.close()
+
+# Continue your program below! 
