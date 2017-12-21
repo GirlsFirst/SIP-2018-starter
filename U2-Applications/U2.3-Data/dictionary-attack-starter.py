f278d9f2714e20c57f95aed8cf493f451958f2ca
SarahGWC <sarah.judd@girlswhocode.com>
2017-12-21 13:16:32 -0500

    Starter code

---
+++ U2-Applications/U2.3-Data/dictionary-attack-starter.py
@@ -0,0 +1,8 @@
+#Opens a file. You can now look at each line in the file individually with a statement like "for line in f:
+f = open("dictionary.txt","r")
+
+print("Can your password survive a dictionary attack?")
+
+#Take input from the keyboard, storing in the variable test_password
+#NOTE - You will have to use .strip() to strip whitespace and newlines from the file and passwords
+test_password = input("Type in a trial password: ")
