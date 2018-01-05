d2c8747366daaebba9fea73cea694f5a3ddc373b
Khevna Shah <khevnashah@gmail.com>
2018-01-05 14:33:26 -0800

    Starter code for Here Kitty Kitty project

---
+++ U2-Applications/U2.1-Robotics/herekittykitty-starter/herekittykitty-starter.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>                           // Include servo library
+
+Servo servoRight;
+Servo servoLeft;
+
+void moveKitty() {
+  // Your Code Here
+}
+
+void stopKitty() {
+  // Your Code Here
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //Your Code Here
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  //Your Code Here
+}
