3e1a4b494dbfc9775975353ea10ab9cac3763972
brknhrt16 <khevnashah@gmail.com>
2018-02-15 11:04:50 -0800

    Update guess_the_number_challenge_starter.py

--- U1-Fundamentals/U1.2-Python/guess_the_number_challenge_starter.py
+++ U1-Fundamentals/U1.2-Python/guess_the_number_challenge_starter.py
@@ -6,8 +6,8 @@ aRandomNumber = randint(1, 20)
 # For Testing: print(aRandomNumber)
 
 guess = input("Guess a number between 1 and 20 (inclusive): ")
-if not guess.isnumeric():
+if not guess.isnumeric(): # checks if a string is only digits 0 to 9
 	print("That's not a positive whole number, try again!")
 else:
-	guess = int(guess) #converts a string to an integer
+	guess = int(guess) # converts a string to an integer
 
