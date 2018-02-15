b311246a9cecbb7878d2e4ca7f4dc0cee2de6b36
Megan Sullivan <meganesulli@gmail.com>
2018-02-15 13:06:26 -0500

    Add main()

--- U1-Fundamentals/U1.2-Python/chatbot/chatbot-starter.py
+++ U1-Fundamentals/U1.2-Python/chatbot/chatbot-starter.py
@@ -2,6 +2,12 @@
 
 
 # --- Put your main program below! ---
-while True:
-  answer = input("(What will you say?) ")
-  print("That's cool!")
+def main():
+  while True:
+    answer = input("(What will you say?) ")
+    print("That's cool!")
+
+
+# DON'T TOUCH! Setup code that runs your main() function.
+if __name__ == "__main__":
+  main()
