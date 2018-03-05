fbad01cf79172d639292b97e7599b42afc39d51b
Diana De los Santos <diana@girlswhocode.com>
2018-03-05 15:12:57 -0800

    Update text_adventure_starter.py

--- U1-Fundamentals/U1.2-Python/text_adventure_starter.py
+++ U1-Fundamentals/U1.2-Python/text_adventure_starter.py
@@ -1,3 +1,5 @@
+
+# Update this text to match your story.
 start = '''
 You wake up one morning and find that you aren't in your bed; you aren't even in your room.
 You're in the middle of a giant maze.
@@ -7,10 +9,14 @@ There is a hallway to your right and to your left.
 
 print(start)
 
-print("Type 'left' to go left or 'right' to go right.")
+print("Type 'left' to go left or 'right' to go right.") # Update to match your story. 
 user_input = input()
 if user_input == "left":
-    print("You decide to go left and...") # finished the story by writing what happens
+    print("You decide to go left and...") # Update to match your story.
+    
+    # Continue code to finish story.
  
 elif user_input == "right":
-    print("You choose to go right and ...") # finished the story writing what happens
+    print("You choose to go right and ...") # Update to match your story.
+    
+    # Continue code to finish story.
