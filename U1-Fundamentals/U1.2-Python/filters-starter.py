e859e6d804e12cbc3632867c04bb9282966c299e
Diana De los Santos <diana@girlswhocode.com>
2018-03-09 13:41:18 -0800

    Created starter code.

---
+++ U1-Fundamentals/U1.2-Python/filters-starter.py
@@ -0,0 +1,25 @@
+# Rename this file to be "filters.py"
+# Add commands to import modules here.
+
+# Define your load_img() function here.
+#       Parameters: The name of the file to be opened (string)
+#       Returns: The image object with the opened file.
+def load_img():
+
+
+# Define your show_img() function here.
+#       Parameters: The image object to display.
+#       Returns: nothing.
+def show_img():
+
+
+# Define your save_img() function here.
+#       Parameters: The image object to save, the name to save the file as (string)
+#       Returns: nothing.
+def save_img():
+
+
+# Define your obamicon() function here.
+#       Parameters: The image object to apply the filter to.
+#       Returns: A New Image object with the filter applied.
+def obamicon():
