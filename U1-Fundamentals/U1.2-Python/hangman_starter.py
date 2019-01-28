a4040b407dbeb06d8716068c338f8343a11e86ba
Khevna Shah <khevnashah@gmail.com>
2019-01-27 23:56:33 -0800

    Added more scaffolding to hangman code

--- U1-Fundamentals/U1.2-Python/hangman_starter.py
+++ U1-Fundamentals/U1.2-Python/hangman_starter.py
@@ -1,14 +1,31 @@
-word = input("Type a word for someone to guess: ")
+import random
+
+# A list of words that 
+potential_words = ["example", "words", "someone", "can", "guess"]
+
+word = random.choice(potential_words)
+
+# Use to test your code:
+# print(word)
 
 # Converts the word to lowercase
 word = word.lower()
 
-# Checks if only letters are present
-if(word.isalpha() == False):
-	print("That's not a word!")
+# Make it a list of letters for someone to guess
+current_word = ["_", "_"] # TIP: the number of letters should match the word
 
 # Some useful variables
 guesses = []
 maxfails = 7
+fails = 0
 
-guess = input("Guess a letter: ")
+while fails < maxfails:
+	guess = input("Guess a letter: ")
+
+	# check if the guess is valid: Is it one letter? Have they already guessed it?
+
+	# check if the guess is correct: Is it in the word? If so, reveal the letters!
+
+	print(current_word)
+
+	fails = fails+1
